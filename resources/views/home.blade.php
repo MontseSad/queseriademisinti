37fe041867fadf0f29dd3f20bcaa12f2c2da4aa8
MontseSad <45673265+MontseSad@users.noreply.github.com>
2018-12-07 02:35:34 -0600

    Update home.blade.php
    Se cambiaron titulos de tablas

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -23,40 +23,46 @@
                     <div class="row">
                         <div class="col-md-3">
                             <div class="panel-content">
-                                <h3 class="heading"><i class="fa fa-square"></i> Today</h3>
+                                <h3 class="heading"><i class="fa fa-square"></i> Hoy</h3>
                                 <ul class="list-unstyled list-justify large-number">
-                                    <li class="clearfix">Earnings <span>$215</span></li>
-                                    <li class="clearfix">Sales <span>47</span></li>
+                                    <li class="clearfix">Ganancias <span>$215</span></li>
+                                    <li class="clearfix">Ventas <span>$47</span></li>
                                 </ul>
                             </div>
                         </div>
                         <div class="col-md-9">
                             <div class="panel-content">
-                                <h3 class="heading"><i class="fa fa-square"></i> Sales Performance</h3>
+                                <h3 class="heading"><i class="fa fa-square"></i> Insumos pendientes para comprar</h3>
                                 <div class="row">
-                                    <div class="col-md-6">
+                                    <div class="table-responsive">
                                         <table class="table">
                                             <thead>
                                                 <tr>
-                                                    <th>&nbsp;</th>
-                                                    <th>Last Week</th>
-                                                    <th>This Week</th>
-                                                    <th>Change</th>
+                                                    <th>Nombre</th>
+                                                    <th>Precio unitario</th>
+                                                    <th>Precio total</th>
+                                                    <th>Estado</th>
+                                                    <th></th>
                                                 </tr>
                                             </thead>
                                             <tbody>
-                                                @foreach($disponibles as $insumo)
+                                                @foreach($insumos as $insumo)
                                                     <tr>
-                                                        <td>{{ $insumo->id }}</td>
                                                         <td>{{ $insumo->nombre }}</td>
                                                         <td>{{ $insumo->precio_u }}</td>
                                                         <td>{{ $insumo->precio_t }}</td>
                                                         <td>{{ $insumo->estado }}</td>
+                                                        <td> <a href="{{ route('insumos.show', $insumo->id) }}">Detalles</a> </td>
+                                                        <!--td><button type="button" 
+                                                            class="btn btn-primary right" 
+                                                            data-toggle="modal" 
+                                                            data-target="#large-modal"
+                                                            data-id="{{ $insumo->id }}">Nuevo insumo</button></td-->
                                                     </tr>
                                                 @endforeach
                                             </tbody>
                                         </table>
-                                        {!! $disponibles->render(); !!}
+                                        {!! $insumos->render(); !!}
                                     </div>
                                 </div>
                             </div>
@@ -65,38 +71,31 @@
                     <div class="row">
                         <div class="col-md-12">
                             <div class="panel-content">
-                                <h3 class="heading"><i class="fa fa-square"></i> Insumos</h3>
+                                <h3 class="heading"><i class="fa fa-square"></i> Insumos disponibles </h3>
                                 @include('layouts.session_message')
                                 <div class="table-responsive">
                                     <table class="table table-striped no-margin">
                                         <thead>
                                             <tr>
-                                                <th>Order No.</th>
-                                                <th>Name</th>
-                                                <th>Amount</th>
-                                                <th>Date &amp; Time</th>
-                                                <th>Status</th>
+                                                <th>Nombre</th>
+                                                <th>Precio unitario</th>
+                                                <th>Precio total</th>
+                                                <th>Estado</th>
                                             </tr>
                                         </thead>
                                         <tbody>
-                                            @foreach($insumos as $insumo)
-                                                <tr>
-                                                    <td>{{ $insumo->id }}</td>
-                                                    <td>{{ $insumo->nombre }}</td>
-                                                    <td>{{ $insumo->precio_u }}</td>
-                                                    <td>{{ $insumo->precio_t }}</td>
-                                                    <td>{{ $insumo->estado }}</td>
-                                                    <td> <a href="{{ route('insumos.show', $insumo->id) }}">Detalles</a> </td>
-                                                    <!--td><button type="button" 
-                                                        class="btn btn-primary right" 
-                                                        data-toggle="modal" 
-                                                        data-target="#large-modal"
-                                                        data-id="{{ $insumo->id }}">Nuevo insumo</button></td-->
-                                                </tr>
-                                            @endforeach
+                                            @foreach($disponibles as $insumo)
+                                                    <tr>
+                                                        <td>{{ $insumo->nombre }}</td>
+                                                        <td>{{ $insumo->precio_u }}</td>
+                                                        <td>{{ $insumo->precio_t }}</td>
+                                                        <td>{{ $insumo->estado }}</td>
+                                                        <td> <a href="{{ route('insumos.show', $insumo->id) }}">Detalles</a> </td>
+                                                    </tr>
+                                                @endforeach
                                         </tbody>
                                     </table>
-                                    {!! $insumos->render(); !!}
+                                    {!! $disponibles->render(); !!}
                                 </div>
                             </div>
                         </div>
